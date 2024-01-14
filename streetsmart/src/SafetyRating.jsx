ba0219ba32b8d22ef19bd991745f4c50cb40a3d1
momoasalu <motunrayoasalu@gmail.com>
2024-01-14 09:54:43 -0500

    Add assets

--- streetsmart/src/SafetyRating.jsx
+++ streetsmart/src/SafetyRating.jsx
@@ -1,16 +1,21 @@
-
+import Low from "./assets/Low.png"
+import High from "./assets/high.png"
+import Moderate from "./assets/moderate.png"
 
 const SafetyRating = ({ranking}) => {
   let icon;
   let label;
   let text;
   if (ranking === "low") {
+    icon = Low;
     label = "low risk";
     text = "take normal safety measures"
   } else if (ranking === "moderate") {
+    icon = Moderate;
     label = "moderate risk";
     text = "take some level of caution"
   } else {
+    icon = High;
     label = "high risk";
     text = "stay away from all travel"
   }
