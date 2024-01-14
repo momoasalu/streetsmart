7dc193560aedc259852a5a84a22782ea4cbe6ab0
momoasalu <motunrayoasalu@gmail.com>
2024-01-14 09:58:34 -0500

    Add Assets

--- streetsmart/src/Summary.jsx
+++ streetsmart/src/Summary.jsx
@@ -1,4 +1,8 @@
 
+import Money from "./assets/money.png";
+import Safety from "./assets/safety.png";
+import Activities from "./assets/activities.png";
+import Food from "./assets/food.png";
 
 const Summary = ({money, safety, activities, food}) => {
   return (
@@ -6,25 +10,25 @@ const Summary = ({money, safety, activities, food}) => {
       <div>
         <h4>money</h4>
         <p>{money}</p>
-        <img src="" alt="" />
+        <img src={Money} alt="" />
       </div>
 
       <div>
         <h4>safety</h4>
         <p>{safety}</p>
-        <img src="" alt="" />
+        <img src={Safety} alt="" />
       </div>
 
       <div>
         <h4>activities</h4>
         <p>{activities}</p>
-        <img src="" alt="" />
+        <img src={Activities} alt="" />
       </div>
 
       <div>
         <h4>food</h4>
         <p>{food}</p>
-        <img src="" alt="" />
+        <img src={Food} alt="" />
       </div>
     </div>
   )
